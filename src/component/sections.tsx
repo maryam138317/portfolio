05e59dbbf930e8fea3591afca1bb9812b36b1b1d
maryam <jafartabarmaryam381@gmail.com>
2026-07-25 10:57:27 -0700

    portfolio

--- src/component/sections.tsx
+++ src/component/sections.tsx
@@ -22,7 +22,7 @@ export default function ProSections({ id, title, children }: { id: string; title
       variants={container}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: true, amount: 0.3 }}
+      viewport={{ once: true, amount: 0.1 }}
     >
       <motion.h3
         variants={fromLeft}
